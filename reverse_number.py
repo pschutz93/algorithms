069bf82e238a75b157a68931096eaf15dfb1b183
Patrick Schutz <patrick@patrickschutz.com>
2014-03-03 17:49:51 -0500

    Python program to reverse a number.

---
+++ reverse_number.py
@@ -0,0 +1,11 @@
+number_to_reverse=int(raw_input("Enter number to reverse: "))
+new_num=0
+length=len(str(number_to_reverse))
+i=0b1
+
+while i<=length:
+	new_num+=(number_to_reverse % 10)*10**(length-i)
+	number_to_reverse/=10
+	i+=1
+
+print new_num
