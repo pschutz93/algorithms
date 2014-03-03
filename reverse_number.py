5f5a26ce9ca4557a03e9ccea7880048cfb54c1c1
pschutz93 <patrick@patrickschutz.com>
2014-03-03 17:52:44 -0500

    Change from binary to base 10

--- reverse_number.py
+++ reverse_number.py
@@ -1,7 +1,7 @@
 number_to_reverse=int(raw_input("Enter number to reverse: "))
 new_num=0
 length=len(str(number_to_reverse))
-i=0b1
+i=1
 
 while i<=length:
 	new_num+=(number_to_reverse % 10)*10**(length-i)
